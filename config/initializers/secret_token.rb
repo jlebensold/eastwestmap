907593358c3fa391d1066006007d5f363a7e574d
Jon Lebensold <jon@lebensold.ca>
2011-08-27 16:08:39 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eastwestmap::Application.config.secret_token = 'deb7514a9b0109e102f005c2bcd6b13275cb58880313689e412dc23e12f5fd25da93ec118fc06543eea6d90e1f2ff5f6252567074b054f269c052708964a6a55'
